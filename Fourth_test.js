8f0a375e7dfbe9df2e6c44ef4d0688fbe3cbafda
Zachariah G. Roppel <zachroppel@legalshieldcorp.com>
2018-02-13 14:57:33 -0600

    Homework 2

---
+++ Fourth_test.js
@@ -0,0 +1,36 @@
+var webdriver = require('selenium-webdriver');
+var $browser = new webdriver.Builder().forBrowser('chrome').build();
+var $driver = webdriver;
+var until = webdriver.until;
+var assert = require('assert');
+var By = $driver.By;
+$browser.manage().window().maximize();
+
+//helpers
+
+var scrollTo = function(selector){
+  var element = $browser.findElement(selector);
+  $browser.executeScript("arguments[0].scrollIntoView()", element);
+};
+
+//end helpers
+
+$browser.get('https://www.legalshield.com').then(function(){
+  return $browser.sleep(3000);
+}).then(function(){
+  return scrollTo(By.css('.media-container'));
+}).then(function(){
+  return $browser.sleep(3000);
+}).then(function(){
+  return $browser.findElement(By.css('a.btn-row:nth-child(1)')).click();
+}).then(function(){
+  return $browser.sleep(3000);
+}).then(function(){
+  return $browser.findElement(By.css('#find-associate-zip-code')).sendKeys('74820');
+}).then(function(){
+  return $browser.findElement(By.css('#find-associate-button')).click();
+}).then(function(){
+  return $browser.sleep(10000);
+}).then(function(){
+  return $browser.quit();
+})
